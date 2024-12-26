bae52ba753f64d14e2c5601010594b527e9dae87
jacky-ai <pathanjilani48@gmail.com>
2024-12-26 14:37:52 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WelcomeComponent, NavbarComponent],
+  imports: [RouterOutlet, WelcomeComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
