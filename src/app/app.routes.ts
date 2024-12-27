539d7cee80884bffb7d4cd7edb4c6dee57b3b17f
jacky-ai <pathanjilani48@gmail.com>
2024-12-28 00:57:04 +0530

    third commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent, title: 'Home Page' },
+  { path: 'sign_up', component: SignUpComponent, title: 'signup' },
 ];
